68a3ee4e0a8943cc24250c499b1f5d2bcc0fba89
Волчугов Антон <toni.volchugov@mail.ru>
2026-04-22 02:59:08 +0300

    feat: добавить адаптив

--- src/pages/home/sections/exchange-rate/ExchangeRate.tsx
+++ src/pages/home/sections/exchange-rate/ExchangeRate.tsx
@@ -1,31 +1,62 @@
-import { Link } from "react-router";
-import "./ExchangeRate.scss";
+import { Link } from 'react-router';
+import './ExchangeRate.scss';
+
+interface IPrice {
+  currency: string;
+  price: number;
+}
+
+const prices: IPrice[] = [
+  { currency: 'USD', price: 60 },
+  { currency: 'JPY', price: 65 },
+  { currency: 'RUB', price: 0.8 },
+  { currency: 'EUR', price: 70 },
+  { currency: 'FRA', price: 62 },
+  { currency: 'TRU', price: 61 },
+];
 
 export const ExchangeRate: React.FC = () => {
+  const mskDate = new Date().toLocaleString('ru-RU', {
+    timeZone: 'Europe/Moscow',
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  });
+
+  const updateString = `Update every 15 minutes, MSC ${mskDate}`;
+
   return (
     <section className="converter" aria-labelledby="converter__title">
-      <div className="converter__left-side">
+      <div className="converter__top">
         <h2 className="converter__title" id="converter__title">
           Exchange rate in internet bank
         </h2>
-
-        <p className="converter__currency-label">Currency</p>
-
-        <Link to={"/"} className="converter__link">
-          All courses
-        </Link>
+        <p className="converter__info">{updateString}</p>
       </div>
 
-      <div className="converter__right-side">
-        <p className="converter__info">
-          Update every 15 minutes, MSC 09.08.2022
-        </p>
+      <div className="converter__middle">
+        <div className="converter__currency-group">
+          <p className="converter__currency-label">Currency</p>
+          <ul className="converter__currency-list">
+            {prices.map(({ currency, price }, index) => (
+              <li key={`${currency}-${index}`} className="converter__currency-item">
+                <span className="converter__currency-name">{currency}:</span>
+                <span className="converter__currency-value">{price}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
         <img
+          alt=""
           src="/images/home-page/bank.svg"
-          alt="bank icon"
           className="converter__bank-img"
+          role="presentation"
         />
       </div>
+
+      <Link to="/" className="converter__bottom">
+        All courses
+      </Link>
     </section>
   );
 };
