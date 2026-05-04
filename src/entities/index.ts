1bbc960987f4a0539cbe2f490a2084ce0864293d
Волчугов Антон <toni.volchugov@mail.ru>
2026-05-04 23:49:37 +0300

    feat: добавить реэкспорт

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,2 +1,3 @@
-export { NewsCard } from "./news";
+export { NewsCard, newsApi } from "./news/index";
+export type { NewsApiArticle, NewsApiResponse } from "./news/index";
 export { currencyApi } from "./currency/index";
--- src/entities/news/index.ts
+++ src/entities/news/index.ts
@@ -1,1 +1,3 @@
 export { NewsCard } from "./ui/news-card/NewsCard.tsx";
+export { newsApi } from "./api/newsApi";
+export type { NewsApiArticle, NewsApiResponse } from "./types/news.types";
