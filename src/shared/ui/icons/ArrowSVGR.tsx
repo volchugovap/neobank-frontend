0b9faac6773ea9834a3563df813ff5c951151f8e
Волчугов Антон <toni.volchugov@mail.ru>
2026-05-04 23:09:28 +0300

    feat: добавить компонент стрелки

---
+++ src/shared/ui/icons/ArrowSVGR.tsx
@@ -0,0 +1,7 @@
+import type { SVGProps } from "react";
+
+export const ArrowSVGR = (props: SVGProps<SVGSVGElement>) => (
+	<svg xmlns="http://www.w3.org/2000/svg" fill="none" {...props}>
+		<path d="M0 8.093h15.158V.7a.2.2 0 0 1 .36-.118L24 12.093l-8.481 11.51a.2.2 0 0 1-.361-.119v-7.391H0" />
+	</svg>
+);
